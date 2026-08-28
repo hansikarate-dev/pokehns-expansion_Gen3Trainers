13b95329e9f6d6c02f8b376d48eeddf2a9c1845b
hansikarate-dev <hansikarate@googlemail.com>
2026-08-28 12:33:02 +0200

    Update moves_info.h

--- src/data/moves_info.h
+++ src/data/moves_info.h
@@ -3843,14 +3843,14 @@ const struct MoveInfo gMovesInfo[MOVES_COUNT_ALL] =
             "Attack that steals half\n"
             "the damage inflicted."),
         .effect = EFFECT_ABSORB,
-        #elif B_UPDATED_MOVE_DATA >= GEN_7
+        #if B_UPDATED_MOVE_DATA >= GEN_7
             .power = 80,
         #else
             .power = 20,
         #endif
         .type = TYPE_BUG,
         .accuracy = 100,
-        #elif B_UPDATED_MOVE_DATA >= GEN_7
+        #if B_UPDATED_MOVE_DATA >= GEN_7
             .pp = 10,
         #else
             .pp = 15,
